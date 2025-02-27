83259afdbfb961a554378f404113cc961fbc0b5c
vrushabh <vrushabhgawde143@gmail.com>
2025-02-27 17:25:09 +0530

    External API hit Using RestTemplate

--- src/main/java/net/engineeringdigest/journalApp/Services/WeatherService.java
+++ src/main/java/net/engineeringdigest/journalApp/Services/WeatherService.java
@@ -3,11 +3,9 @@ package net.engineeringdigest.journalApp.Services;
 import net.engineeringdigest.journalApp.apiResponse.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
@@ -19,12 +17,20 @@ public class WeatherService {
     private RestTemplate restTemplate;
 
     public WeatherResponse getWeather(String city) {
-    String uriString = UriComponentsBuilder.fromHttpUrl(url)
-            .queryParam("key",apiKey)
-            .queryParam("q",city)
-            .toUriString();
+        String uriString = UriComponentsBuilder.fromHttpUrl(url)
+                .queryParam("key", apiKey)
+                .queryParam("q", city)
+                .toUriString();
 
-        ResponseEntity<WeatherResponse>response= restTemplate.exchange(uriString, HttpMethod.GET,null, WeatherResponse.class);
+        ResponseEntity<WeatherResponse> response = restTemplate.exchange(uriString, HttpMethod.GET, null, WeatherResponse.class);
         return response.getBody();
     }
+
+//    public WeatherResponse setWeather(Users user) { this is demo code for performing POST to external api. Dont uncomment it will not run
+//    HttpHeaders httpHeaders = new HttpHeaders();
+//    httpHeaders.set("key",value);
+//    HttpEntity<Users> requestEntity = new HttpEntity<>(user,httpHeaders);
+//        ResponseEntity<WeatherResponse>response= restTemplate.exchange(uriString, HttpMethod.POST,requestEntity, WeatherResponse.class);
+//        return response.getBody();
+//    }
 }
